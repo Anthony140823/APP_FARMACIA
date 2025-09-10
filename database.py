c9643928174dc30be749c2f5bc90644f04e3a672
Anthony140823 <110640828+Anthony140823@users.noreply.github.com>
2025-09-10 01:02:09 -0500

    Update database.py

--- database.py
+++ database.py
@@ -14,10 +14,10 @@ class Database:
     def connect(self):
         try:
             self.connection = mysql.connector.connect(
-                host=os.getenv("MYSQLHOST", "localhost"),
-                database=os.getenv("MYSQLDATABASE", "farmacia_db"),
-                user=os.getenv("MYSQLUSER", "root"),
-                password=os.getenv("MYSQLPASSWORD", "admin"),
+                host=os.getenv("MYSQLHOST", "btkgltlwtqlihq6ytfh6-mysql.services.clever-cloud.com"),
+                database=os.getenv("MYSQLDATABASE", "btkgltlwtqlihq6ytfh6"),
+                user=os.getenv("MYSQLUSER", "upazlqy3bnwfzt9h"),
+                password=os.getenv("MYSQLPASSWORD", "tBmoYwbesqroW7Xf38PE"),
                 port=int(os.getenv("MYSQLPORT", 3306))
             )
             if self.connection.is_connected():
